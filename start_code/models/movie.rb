64e655f108b227ddbf5a8812fb83ecb524aae314
Cameron Moir <cameronjmoir@gmail.com>
2019-09-26 15:48:02 +0100

    added find star by movie function

--- start_code/models/movie.rb
+++ start_code/models/movie.rb
@@ -36,6 +36,18 @@ class Movie
           SqlRunner.run(sql, values)
         end
 
+        def stars()
+          sql = "SELECT stars.*
+          FROM stars
+          INNER JOIN castings
+          ON stars.id = castings.star_id
+          WHERE castings.movie_id = $1"
+          values = [@id]
+          star_hashes = SqlRunner.run(sql, values)
+          result = star_hashes.map { |star_hash| Star.new(star_hash)  }
+          return result
+        end
+
         def self.delete_all()
           sql = "DELETE FROM movies"
           SqlRunner.run(sql)
